72259005ee32d47ca926c2ad80c4b56ce269f0f2
Sebastien Corbin <seb.corbin@gmail.com>
2016-01-12 13:47:48 +0100

    Working template

---
+++ TextualMeta/TPIMeta.h
@@ -0,0 +1,14 @@
+//
+//  TPIMeta.h
+//  TextualMeta
+//
+//  Created by SebCorbin on 12/01/2016.
+//  Copyright © 2016 SebCorbin. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "TextualApplication.h"
+
+@interface TPIMeta : NSObject <THOPluginProtocol>
+
+@end
---
+++ TextualMeta/TPIMeta.m
@@ -0,0 +1,19 @@
+//
+//  TPIMeta.m
+//  TextualMeta
+//
+//  Created by SebCorbin on 12/01/2016.
+//  Copyright © 2016 SebCorbin. All rights reserved.
+//
+
+#import "TPIMeta.h"
+
+@implementation TPIMeta
+
+- (NSString*)processInlineMediaContentURL:(NSString *)resource {
+    
+    
+    return nil;
+}
+
+@end
