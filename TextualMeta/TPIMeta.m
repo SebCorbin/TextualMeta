0bfac110b421ce17dc6e63d44749d52f55600235
Sebastien Corbin <seb.corbin@gmail.com>
2016-01-12 15:32:35 +0100

    It works!

--- TextualMeta/TPIMeta.m
+++ TextualMeta/TPIMeta.m
@@ -7,11 +7,34 @@
 //
 
 #import "TPIMeta.h"
+#import "HTMLParser/HTMLParser.h"
 
 @implementation TPIMeta
 
 - (NSString*)processInlineMediaContentURL:(NSString *)resource {
+    // Try to load the html from resource
+    NSError* error = nil;
+    NSStringEncoding encoding;
+    NSString* html = [NSString stringWithContentsOfURL:[NSURL URLWithString:resource] encoding:encoding error:&error];
+    if (error) {
+        return nil;
+    }
+
+    // Parse the html
+    HTMLParser *parser = [[HTMLParser alloc] initWithString:html error:&error];
+    if (error) {
+        return nil;
+    }
     
+    HTMLNode *headNode = [parser head];
+    
+    NSArray *metaTags = [headNode findChildTags:@"meta"];
+    
+    for (HTMLNode *inputNode in metaTags) {
+        if ([[inputNode getAttributeNamed:@"property"] isEqualToString:@"og:image"]) {
+            return [inputNode getAttributeNamed:@"content"];
+        }
+    }
     
     return nil;
 }
